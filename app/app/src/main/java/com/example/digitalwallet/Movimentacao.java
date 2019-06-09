92269e32c04df869f4aa20c3504f87abcfe0b3be
GustavoBellettini <gustavobellettini@gmail.com>
2019-06-09 17:10:55 -0300

    Front

---
+++ app/app/src/main/java/com/example/digitalwallet/Movimentacao.java
@@ -0,0 +1,28 @@
+package com.example.digitalwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Movimentacao extends AppCompatActivity{
+
+    private String array_spinner[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movimentacao);
+
+        array_spinner = new String[2];
+        array_spinner[0]="Receita";
+        array_spinner[1]="Despesa";
+        Spinner s = (Spinner) findViewById(R.id.tipoMovSpinner);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, array_spinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+
+}
