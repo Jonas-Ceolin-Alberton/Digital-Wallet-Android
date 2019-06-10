500755284bbcce78321b686e9beab04238ca486e
GustavoBellettini <gustavobellettini@gmail.com>
2019-06-09 23:18:51 -0300

    Att

--- app/app/src/main/java/movimentacoes/MovimentacaoActivity.java
+++ app/app/src/main/java/movimentacoes/MovimentacaoActivity.java
@@ -1,9 +1,12 @@
 package movimentacoes;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -12,11 +15,25 @@ import com.example.digitalwallet.R;
 public class MovimentacaoActivity extends AppCompatActivity{
 
     private String array_spinner[];
+    private DrawerLayout ndrawerLayout;
+    private ActionBarDrawerToggle ntoggle;
+    private Toolbar nToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movimentacao);
+
+        nToolbar = (Toolbar) findViewById(R.id.tipoOrigem);
+        setSupportActionBar(nToolbar);
+
+        ndrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ntoggle = new ActionBarDrawerToggle(this, ndrawerLayout, R.string.abrir, R.string.fechar);
+
+        ndrawerLayout.addDrawerListener(ntoggle);
+        ntoggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         array_spinner = new String[2];
         array_spinner[0]="Receita";
@@ -28,6 +45,16 @@ public class MovimentacaoActivity extends AppCompatActivity{
 
         Movimentacao mov =  new Movimentacao();
         mov.setTipoMovimentacao(Movimentacao.TipoMovimentacao.DESPESA);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(ntoggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
